175ac88c00965bb6e9b4d80c895f0c9dfa43cb95
cabrinhaFx <dom.mowius@outlook.de>
2016-08-15 20:52:36 +0200

    Synced files

---
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Pawn.h"
+#include "Tank.generated.h" // must be last #include
+
+UCLASS()
+class BATTLETANK_API ATank : public APawn
+{
+	GENERATED_BODY()
+
+private:
+	// Sets default values for this pawn's properties
+	ATank();
+};
---
+++ BattleTank/Source/BattleTank/Public/TankBarrel.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/StaticMeshComponent.h"
+#include "TankBarrel.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent)) //, hidecategories = ("Collision"))
+class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	void Elevate(float RelativeSpeed);
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = Setup)
+	float MaxDegreesPerSecond = 20.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = Setup)
+	float MaxElevationDegrees = 40.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category = Setup)
+	float MinElevationDegrees = 0.f;	
+};
